b03620519efd207233b2040fa61d1b0b6c9df0d8
ivan <ivan.shpotenko@gmail.com>
2019-06-05 17:12:05 +0300

    binary search

--- exercises/binary-search/binary_search.exs
+++ exercises/binary-search/binary_search.exs
@@ -18,6 +18,20 @@ defmodule BinarySearch do
   """
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
+  def search({}, _), do: :not_found
   def search(numbers, key) do
+    search(numbers, key, 0, tuple_size(numbers) - 1)
   end
+
+  def search(_numbers, _key, first_i, last_i) when first_i > last_i, do: :not_found
+  def search(numbers, key, first_i, last_i) do
+    middle = middle_index(first_i, last_i)
+    case elem(numbers, middle) do
+      ^key -> {:ok, middle}
+      n when key < n -> search(numbers, key, first_i, middle - 1)
+      n when key > n -> search(numbers, key, middle + 1, last_i)
+    end
+  end
+
+  def middle_index(first_i, last_i), do: floor((last_i + first_i) / 2)
 end
